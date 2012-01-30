conformal/xombrero

commit e1e31b4e2e1329beb84cf73f94480ff82f71db54

convert long long to int to shut mingw up

xxxterm.c
@@ -5144,7 +5144,7 @@ hint_continue(struct tab *t)
 	char			*s;
 	const gchar		*errstr = NULL;
 	gboolean		rv = TRUE;
-	long long		i;
+	int			i;
 
 	if (!(c[0] == '.' || c[0] == ','))
 		goto done;
@@ -5161,7 +5161,7 @@ hint_continue(struct tab *t)
 			show_oops(t, "invalid numerical hint %s", &c[1]);
 			goto done;
 		}
-		s = g_strdup_printf("hints.updateHints(%lld);", i);
+		s = g_strdup_printf("hints.updateHints(%d", i);
 		run_script(t, s);
 		g_free(s);
 	} else {

commit 9f59d79fc0b4c0ea90f924c889423525ba46ee1b

Fix problem with hinting introduced by e1e31b4e

xxxterm.c
@@ -5220,7 +5220,7 @@ hint_continue(struct tab *t)
 			show_oops(t, "invalid numerical hint %s", &c[1]);
 			goto done;
 		}
-		s = g_strdup_printf("hints.updateHints(%d", i);
+		s = g_strdup_printf("hints.updateHints(%d);", i);
 		run_script(t, s);
 		g_free(s);
 	} else {