7a2f4d931d564cc76dd3e9fd590e841b737bf4d7
Marco Peereboom <marco@conformal.com>
2012-01-31 18:20:28 -0600

    add unix specific file

---
+++ unix.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2010, 2011 Marco Peereboom <marco@peereboom.us>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <xxxterm.h>
+
+int
+fork_exec(struct tab *t, char *argv0, const gchar *argv1, char *error)
+{
+	switch (fork()) {
+	case -1:
+		show_oops(t, error);
+		return (1);
+		/* NOTREACHED */
+	case 0:
+		break;
+	default:
+		return (0);
+	}
+
+	/* child */
+	execlp(argv0, argv0, argv1, (void *)NULL);
+
+	_exit(0);
+
+	/* NOTREACHED */
+
+	return (0);
+}
