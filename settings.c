2c0869fdf6a4298679e0eb512064d33dddd03b03
Josh Rickmar <jrick@devio.us>
2012-05-07 11:08:23 -0400

    enable_autoscroll is an action, not a setting.  The old code was correct.

--- settings.c
+++ settings.c
@@ -1101,16 +1101,7 @@ set_auto_load_images(char *value)
 int
 set_enable_autoscroll(char *value)
 {
-	struct tab              *t;
-
 	enable_autoscroll = atoi(value);
-	if (enable_autoscroll) {
-		TAILQ_FOREACH(t, &tabs, entry) {
-			run_script(t, JS_AUTOSCROLL);
-		}
-	} else {
-		/* XXX: kill the script if running */
-	}
 	return (0);
 }
 
